51d06b54e5f96cc60aa4f0730505bcc3d602c70f
Holger <holger.trittenbach@gmail.com>
2018-11-19 12:31:46 +0100

    Add documentation for labelmap2vec.

--- src/learnbase.jl
+++ src/learnbase.jl
@@ -361,3 +361,27 @@ frequencies for each label in `obj`
       1 => 3
 """
 function labelfreq! end
+
+"""
+    labelmap2vec(dict) -> Vector
+
+Inverse function of labelmap.
+Computes an `array` of labels by element-wise
+traversal of the entries in `dict`.
+
+    julia> labelvec = [:yes,:no,:no,:yes,:yes]
+
+    julia> lm = labelmap(labelvec)
+    Dict{Symbol,Array{Int64,1}} with 2 entries:
+        :yes => [1, 4, 5]
+        :no  => [2, 3]
+
+    julia> labelmap2vec(lm)
+    5-element Array{Symbol,1}:
+        :yes
+        :no
+        :no
+        :yes
+        :yes
+"""
+function labelmap end
